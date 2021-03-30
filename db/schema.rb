30f8d9b92b11fa4cd461882e495d2ecb40160421
patelsimran2104 <simran.patel@bacancy.com>
2021-03-30 15:48:08 +0530

    locally psql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_02_24_113819) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "results", force: :cascade do |t|
     t.integer "student_id", null: false
     t.integer "Studentinfo_id"
